84f74e5ee9a4493db7e95df9a4a65abbdb2f6339
VVNguyen <vovannguyen0123@gmail.com>
2024-02-21 21:16:04 +0700

    init repository

---
+++ src/main/java/org/example/question_1_2/Question12Application.java
@@ -0,0 +1,13 @@
+package org.example.question_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Question12Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/question_1_2/Question12ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.question_1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Question12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
