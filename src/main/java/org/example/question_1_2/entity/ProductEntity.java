3bfc83b45e81bb988076c44d8847c3c790241652
VVNguyenBV <nguyen.vo-van@banvien.com.vn>
2024-02-22 17:07:55 +0700

    updated source code

--- src/main/java/org/example/question_1_2/entity/ProductEntity.java
+++ src/main/java/org/example/question_1_2/entity/ProductEntity.java
@@ -1,7 +1,10 @@
 package org.example.question_1_2.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +14,23 @@ import lombok.Setter;
 @Table(name = "products")
 public class ProductEntity extends BaseEntity{
 
+    @Column(name = "name" )
+    @NotBlank
     private String name;
 
+    @Column(name = "quantity")
+    @NotBlank
     private Integer quantity;
 
+    @Column(name = "price")
+    @NotNull
     private Float price;
 
+    @Column(name = "madeIn")
+    @NotNull
+    private String madeIn;
+
+    @Column(name = "description")
+    @NotBlank
     private String description;
 }
